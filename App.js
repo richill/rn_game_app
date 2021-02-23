b621db7225be5b0740b6095ce6d9cca5fa87607a
Richill Tamakloe <richill.tamakloe@gmail.com>
2021-02-23 10:06:44 +0000

    Create component header props title and styling

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import Header from './components/Header';
 
 export default function App() {
   return (
-    <View>
+    <View style={styles.screen}>
+      <Header
+        title="Game App"
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-
+  screen: {
+    flex: 1
+  }
 });
