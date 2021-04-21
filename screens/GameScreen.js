92d0b0b52373feec4103be2d67c53f0e968a5d9f
Richill Tamakloe <richill.tamakloe@gmail.com>
2021-04-21 09:21:35 +0100

    fix issue with alert not displaying

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,27 +1,23 @@
 import React, { useState, useRef } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  Button,
-  Alert
-} from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 import NumberContainer from '../components/NumberContainer';
 import Card  from '../components/Card';
 
 
-const generateRandombetween = (min, max, exclude) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  const rndNum = Math.floor(Math.random() * (max - min)) + min;
-  if (rndNum === exclude) {
-    return generateRandombetween(min, max, exclude);
-  } else {
-    return  rndNum;
-  }
-}
+
 
 const GameScreen = (props) => {
+  const generateRandombetween = (min, max, exclude) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    const rndNum = Math.floor(Math.random() * (max - min)) + min;
+    if (rndNum === exclude) {
+      return generateRandombetween(min, max, exclude);
+    } else {
+      return  rndNum;
+    }
+  }
+
   const [currentGuess, setCurrentGuesss] = useState(
     generateRandombetween(1, 100, props.userChoice)
   );
@@ -32,13 +28,14 @@ const GameScreen = (props) => {
   const nextGuesssHandler = (direction) => {
     if ((direction === 'lower' && currentGuess < props.userChoice) || (direction === 'greater' && currentGuess > props.userChoice)) {
       Alert.alert(
-        'Don\'t lie', 'in giving the Opponent the wrong direction',
-        [{text: 'Sorry!', style:'cancel'}]
+        'Do not lie',
+        'in giving the Opponent the wrong direction',
+        [{ text: 'Sorry!', style:'cancel' }]
       );
       return;
-    }
+    };
 
-    if (direction == 'lower') {
+    if (direction === 'lower') {
       currentHigh.current = currentGuess;
     } else {
       currentLow.current = currentGuess;
