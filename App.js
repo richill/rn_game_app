79d7c3cc05b10f312ed19868043e295026a5c7b6
Richill Tamakloe <richill.tamakloe@gmail.com>
2021-05-04 10:18:00 +0100

    Add font and App loading

--- App.js
+++ App.js
@@ -1,13 +1,39 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
+// import { AppLoading } from 'expo-app-loading';
+// import * as Font from 'expo-font';
+
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
 import Header from './components/Header';
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
 import GameOverScreen from './screens/GameOverScreen';
 
+// const [fetchFonts] = () => {
+//   Font.loadAsync({
+//     'open-sans': require('./assets/Fonts/OpenSans-Regular.ttf'),
+//     'open-sans-bold': require('./assets/Fonts/OpenSans-Bold.ttf'),
+//   });
+// };
+// added outside of the functional components
+// because this function does not need to be recreated for every component re-render cycle
+
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [guessRounds, setGuessRounds] = useState(0);
+  // const [dataLoaded, setDataLoaded] = useState(false);
+
+  // if (!dataLoaded) {
+  //   return (
+  //     <AppLoading 
+  //       startAsync={fetchFonts}
+  //       onFinish={() => setDataLoaded(true)}
+  //     />
+  //   );
+  // }
+  // This function will be excuted when the fetchFonts is done
 
   const configureNewGameHandler = () => {
     setGuessRounds(0);
@@ -30,12 +56,22 @@ export default function App() {
     content = <GameOverScreen roundsNumber={guessRounds} computerGuessNumber={userNumber} onRestart={configureNewGameHandler}/>
   }
 
-  return (
-    <View style={styles.screen}>
-      <Header title="Game App" />
-      {content}
-    </View>
-  );
+  let [fontsLoaded] = useFonts({
+    "open-sans": require('./assets/Fonts/OpenSans-Regular.ttf'),
+    "open-sans-bold": require('./assets/Fonts/OpenSans-Bold.ttf'),
+  });
+
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <View style={styles.screen}>
+        <Header title="Game App" />
+        {content}
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -43,6 +79,11 @@ const styles = StyleSheet.create({
     flex: 1
   }
 });
+
+// AppLoading: it will prolong the default loading screen to stay active
+// until untill all content of a certain task of my choice has been done or rendered
+// related to dataloaded & setDataLoaded
+// must install by running -  expo install expo-app-loading or npm install --save expo-app-loading
 
 // notes: styling: 
 // https://reactnative.dev/docs/components-and-apis
@@ -53,3 +94,4 @@ const styles = StyleSheet.create({
 
 // expo install expo-app-loading
 // import AppLoading from 'expo-app-loading';
+
