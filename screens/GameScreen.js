119623d82e2b50a8eeb96a39f745a940d822b671
Richill Tamakloe <richill.tamakloe@gmail.com>
2021-06-14 09:40:11 +0100

    scrollView adn flexBox

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -87,8 +87,8 @@ const GameScreen = (props) => {
           <Ionicons name="md-add" size={24} color="white"/>
         </MainButton>
       </Card>
-      <View style={styles.list}>
-        <ScrollView>
+      <View style={styles.listContainer}>
+        <ScrollView contentContainerStyle={styles.list}>
           {pastGuesses.map((guess, index) => renderListItem(guess, pastGuesses.length - index))}
         </ScrollView>
       </View>
@@ -109,9 +109,14 @@ const styles = StyleSheet.create({
     width: 400,
     maxWidth: '90%'
   },
-  list: {
+  listContainer: {
     flex: 1,
     width: '80%'
+  },
+  list: {
+    flexGrow: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
   },
   listItem: {
     borderColor: '#ccc',
@@ -120,7 +125,8 @@ const styles = StyleSheet.create({
     marginVertical: 10,
     backgroundColor: 'white',
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    width: '60%'
   }
 });
 
