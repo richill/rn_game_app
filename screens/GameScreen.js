e93b159ca14b68f692424244289ea7bb6aac5d29
Richill Tamakloe <richill.tamakloe@gmail.com>
2021-06-14 09:28:04 +0100

    Styling list items & lists

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,10 +1,18 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { StyleSheet, Text, View, Alert, ScrollView } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import NumberContainer from '../components/NumberContainer';
 import Card  from '../components/Card';
-import DefaultStyles from '../constants/default-styles';
 import MainButton from '../components/MainButton';
-import { Ionicons } from '@expo/vector-icons';
+import BodyText from '../components/BodyText';
+import DefaultStyles from '../constants/default-styles';
+
+const renderListItem = (value, numOfRounds) => (
+  <View key={value} style={styles.listItem}>
+    <BodyText>#{numOfRounds}</BodyText>
+    <BodyText>{value}</BodyText>
+  </View>
+);
 
 const GameScreen = (props) => {
   const generateRandombetween = (min, max, exclude) => {
@@ -79,13 +87,11 @@ const GameScreen = (props) => {
           <Ionicons name="md-add" size={24} color="white"/>
         </MainButton>
       </Card>
-      <ScrollView>
-        {pastGuesses.map(guess => (
-          <View key={guess}>
-            <Text>{guess}</Text>
-          </View>
-        ))}
-      </ScrollView>
+      <View style={styles.list}>
+        <ScrollView>
+          {pastGuesses.map((guess, index) => renderListItem(guess, pastGuesses.length - index))}
+        </ScrollView>
+      </View>
     </View>
   );
 };
@@ -102,6 +108,19 @@ const styles = StyleSheet.create({
     marginTop: 20,
     width: 400,
     maxWidth: '90%'
+  },
+  list: {
+    flex: 1,
+    width: '80%'
+  },
+  listItem: {
+    borderColor: '#ccc',
+    borderWidth: 1,
+    padding: 12,
+    marginVertical: 10,
+    backgroundColor: 'white',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
   }
 });
 
