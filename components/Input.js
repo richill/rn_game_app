cdd89bff8ba236a201cbce31d2d36b6ad7cead26
Richill Tamakloe <richill.tamakloe@gmail.com>
2022-01-06 18:33:45 +0000

    Update notes on props

--- components/Input.js
+++ components/Input.js
@@ -20,6 +20,9 @@ const styles = StyleSheet.create({
 
 // notes:
 // { ...props }: fowarding your props to the component you are using
+// { ...props }: allows one to custome their input.js props in which ever compenent its being called in
+// eg: in StartGameScreen i want my input props to be autocorrect, keyboardType, maxlength & in AnotherScreen i want my input props to only have autocorrect 
+
 // takes all your props & adds them to your component
 
 // {{ ...props.style}}: allows you to have additional styling for 
