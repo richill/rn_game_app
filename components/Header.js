51a0f0309f8e866bd5dc62f7a40e57dec2f4eb3d
Richill Tamakloe <richill.tamakloe@gmail.com>
2022-02-16 11:23:55 +0000

    Update IOS headr colour

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform } from 'react-native';
 import Colours from '../constants/colours';
 import TitleText from './TitleText';
 
@@ -7,7 +7,7 @@ const Header = (props) => {
   return(
     <View style={styles.header}>
       {/*<Text style={styles.headerTitle}>{props.title}</Text>*/}
-      <TitleText style={styles.headerTitle}>{props.title}</TitleText>
+      <TitleText style={styles.title}>{props.title}</TitleText>
     </View>
   );
 };
@@ -17,14 +17,20 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 90,
     paddingTop: 36,
-    backgroundColor: Colours.primiary,
+    backgroundColor: Platform.OS === 'android' ? Colours.primiary : 'white',
+    // note: platform allows user to set same styling as android
+    // if my platform is not android and it is an IOS set the background colour to white
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    borderBottomColor: Platform.OS === 'ios' ? '#ccc' : 'transparent',
+    borderBottomWidth: Platform.OS === 'ios' ? 1 : 0,
+    //note: if the platform is ios set it #ccc if not set it white
+    //note: if the platform is ios set it 1 if not set it 0 (androids will be set to 0 as it is not an iOS styling)
   },
-  // headerTitle: {
-  //   color: 'black',
-  //   fontSize: 18
-  // }
+  title: {
+    color: Platform.OS === 'ios' ? Colours.primiary : 'white',
+    fontSize: 18
+  }
 });
 
 export default Header;
